b3f48e96de05caa3235d2c2a423443dd2d355635
Mikhail Privalov <n5250@ya.ru>
2021-11-06 02:41:03 +0300

    Update client.py

--- esia/client.py
+++ esia/client.py
@@ -391,6 +391,14 @@ class EsiaInformationConnector(object):
         url = '{base}/prns/{oid}/kids?embed=(elements)'.format(
             base=self._rest_base_url, oid=self.oid)
         return self.esia_request(endpoint_url=url, accept_schema=accept_schema)
+    
+    def get_kid_documents(self, kid_oid, accept_schema=None):
+        """
+        Возвращает документы ребенка
+        :rtype: dict
+        """
+        url = f'{base}/prns/{oid}/kids/{kid_oid}/docs?embed=(documents.elements)'
+        return self.esia_request(endpoint_url=url, accept_schema=accept_schema)
 
     def get_person_transport(self, accept_schema=None):
         """
